3f40b53077337e2b1813c47d85b8268c61bd34a4
replay <replaykim123@gmail.com>
2016-06-24 21:36:41 +0900

    edit

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+var NoteApp = {
+    $wrap: $(document.body),
+    storageKey : 'note'
+};
+
 (function(app){
 
   app.model = {
@@ -25,44 +30,6 @@
   };
 })(NoteApp, jQuery);
 
-(function($, app){
-
-	console.log('my-note app.js');
-	var $header = $('.header');
-	var $memo = $('#memo')
-	var initData = app.util.storage.load();
-
-	
-	
-	$header.on('click', '.btn-fullscreen', function(event){
-
-		app.BtnAction.fullScreen();
-
-	});
-
-	$header.on('click','.btn-about' , function(){
-
-		app.BtnAction.about();
-	});
-
-	$header.on('click','.btn-savenote' ,function(){
-
-		app.view.addTodo();
-
-	});
-
-	$header.on('click', '.btn-newnote' ,function(){
-		$memo.val('');
-		
-	});
-
-	if(initData.length >= 1) {
-		console.log(initData)
-		var index = initData.length-1;
-		$memo.val(initData[index].value);
-		// app.collection.set( initData );
-	}
-})(jQuery,NoteApp)
 (function(app){
 
 
@@ -98,11 +65,6 @@
   // app.ButtonAction = BtnAction;
 
 })(NoteApp);
-
-var NoteApp = {
-    $wrap: $(document.body),
-    storageKey : 'note'
-};
 
 
 (function($, app){
@@ -159,3 +121,42 @@ var NoteApp = {
 
 })(NoteApp);
 
+
+(function($, app){
+
+	console.log('my-note app.js');
+	var $header = $('.header');
+	var $memo = $('#memo')
+	var initData = app.util.storage.load();
+
+	
+	
+	$header.on('click', '.btn-fullscreen', function(event){
+
+		app.BtnAction.fullScreen();
+
+	});
+
+	$header.on('click','.btn-about' , function(){
+
+		app.BtnAction.about();
+	});
+
+	$header.on('click','.btn-savenote' ,function(){
+
+		app.view.addTodo();
+
+	});
+
+	$header.on('click', '.btn-newnote' ,function(){
+		$memo.val('');
+		
+	});
+
+	if(initData.length >= 1) {
+		console.log(initData)
+		var index = initData.length-1;
+		$memo.val(initData[index].value);
+		// app.collection.set( initData );
+	}
+})(jQuery,NoteApp)
